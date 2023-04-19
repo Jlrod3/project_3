88994009fd790dcc5387ead7e7ddb47ae287669c
Jlrod3 <112913660+Jlrod3@users.noreply.github.com>
2023-04-18 21:07:59 -0500

    Adding more Javascript

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 let slides = document.getElementsByClassName("slider__slide");
 let navlinks = document.getElementsByClassName("slider__navlink");
+let currentSlide = 0;
+
+document.getElementById("nav-button--next").addEventListener("click", () => {
+    changeSlide(currentSlide + 1)
+});
+document.getElementById("nav-button--prev").addEventListener("click", () => {
+    changeSlide(currentSlide - 1)
+});
+
+document.getElementById("nav-button--next").addEventListener("click", changeSlide(currentSlide + 1));
+
