ae51c492dd1fc3a14359fd3c7701a41e4631ef69
Jlrod3 <112913660+Jlrod3@users.noreply.github.com>
2023-04-18 21:09:31 -0500

    make it all clickable!

--- js/script.js
+++ js/script.js
@@ -11,3 +11,22 @@ document.getElementById("nav-button--prev").addEventListener("click", () => {
 
 document.getElementById("nav-button--next").addEventListener("click", changeSlide(currentSlide + 1));
 
+function changeSlide(moveTo) {
+    if (moveTo >= slides.length) {moveTo = 0;}
+    if (moveTo < 0) {moveTo = slides.length - 1;}
+    
+    slides[currentSlide].classList.toggle("active");
+    navlinks[currentSlide].classList.toggle("active");
+    slides[moveTo].classList.toggle("active");
+    navlinks[moveTo].classList.toggle("active");
+    
+    currentSlide = moveTo;
+}
+
+document.querySelectorAll('.slider__navlink').forEach((bullet, bulletIndex) => {
+    bullet.addEventListener('click', () => {
+        if (currentSlide !== bulletIndex) {
+            changeSlide(bulletIndex);
+        }
+    })
+})
